78e5d36027d852dd4f3be41ff1646a22f95d582d
Maranderine <104585818+Maranderine@users.noreply.github.com>
2024-05-16 11:12:30 +0200

    started apiService

---
+++ app/src/main/java/de/hsb/greenquest/data/LocalChallenge.kt
@@ -0,0 +1,16 @@
+package de.hsb.greenquest.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "challenges")
+data class LocalChallenge (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val description: String,
+    val Plant: String,
+    val requiredCount: Int,
+    val progress: Int,
+    val date: Date
+)
