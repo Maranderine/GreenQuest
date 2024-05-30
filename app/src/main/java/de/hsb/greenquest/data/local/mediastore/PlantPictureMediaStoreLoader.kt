59f67aaec96720d0918d670eed0bf3832602ab26
Thai1337 <47648462+Thai1337@users.noreply.github.com>
2024-05-30 14:48:45 +0200

    cool code that does nothing :cry:

--- app/src/main/java/de/hsb/greenquest/data/local/mediastore/PlantPictureMediaStoreLoader.kt
+++ app/src/main/java/de/hsb/greenquest/data/local/mediastore/PlantPictureMediaStoreLoader.kt
@@ -2,21 +2,50 @@ package de.hsb.greenquest.data.local.mediastore
 
 import android.content.ContentUris
 import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
+import android.util.Log
 import de.hsb.greenquest.domain.model.Plant
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class PlantPictureMediaStoreLoader @Inject constructor(
     private val applicationContext: Context
 )/*: PlantPictureRepository*/ {
+
+    private val _plantPicturesFlow = MutableStateFlow<MutableList<Plant>>(mutableListOf())
+    val plantPicturesFlow: StateFlow<MutableList<Plant>> = _plantPicturesFlow
+
+    init {
+        val contentObserver = object : ContentObserver(Handler(Looper.getMainLooper())) {
+            override fun onChange(selfChange: Boolean) {
+                loadPicturesFromMediaStore()
+            }
+        }
+
+        applicationContext.contentResolver.registerContentObserver(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            true,
+            contentObserver
+        )
+
+        // Initial load
+        loadPicturesFromMediaStore()
+    }
+
     /*override*/ suspend fun savePlantPicture(plant: Plant) {
-        //TODO("Not yet implemented")
+        // TODO: Implement save functionality
     }
 
     /*override*/ fun getAllPlantPictures(): Flow<MutableList<Plant>> {
-        return loadPicturesFromMediaStore()
+        return plantPicturesFlow
     }
 
     fun deletePlantPicture(plant: Plant) {
@@ -27,7 +56,7 @@ class PlantPictureMediaStoreLoader @Inject constructor(
         }
     }
 
-    private fun loadPicturesFromMediaStore(): Flow<MutableList<Plant>> {
+    private fun loadPicturesFromMediaStore() {
         val context = applicationContext
         val contentResolver = context.contentResolver
 
@@ -42,46 +71,37 @@ class PlantPictureMediaStoreLoader @Inject constructor(
         val selection = MediaStore.Images.Media.RELATIVE_PATH + " LIKE ?"
         val selectionArgs = arrayOf("Pictures/GreenQuest/")
 
-        // Return a flow of lists of Plant objects
-        return flow {
-            val plantImageList: MutableList<Plant> = mutableListOf()
+        // Query the MediaStore
+        val plantImageList: MutableList<Plant> = mutableListOf()
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            null
+        )?.apply {
+            val idColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val nameColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
+            val pathColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.RELATIVE_PATH)
+            val isFavoriteColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.IS_FAVORITE)
 
-            // Query the MediaStore
-            contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection,
-                selection,
-                selectionArgs,
-                null
-            )?.apply {
-                val idColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-                val nameColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
-                val pathColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.RELATIVE_PATH)
-                val isFavoriteColumnIndex = getColumnIndexOrThrow(MediaStore.Images.Media.IS_FAVORITE)
+            // Process each row in the cursor
+            while (moveToNext()) {
+                val id = getLong(idColumnIndex)
+                val name = getString(nameColumnIndex)
+                val favorite: Boolean = getString(isFavoriteColumnIndex).toInt() != 0
+                val contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                // Process each row in the cursor
-                while (moveToNext()) {
-                    val id = getLong(idColumnIndex)
-                    val name = getString(nameColumnIndex)
-                    val favorite: Boolean = getString(isFavoriteColumnIndex).toInt() != 0
-                    val contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-                    // Add the Plant object to the list
-                    plantImageList.add(Plant(name = name, imagePath = contentUri, favorite = favorite, description = "", commonNames = listOf(), species = ""))
-
-//                    Log.d("TESTTEST", "DISPLAY_NAME = $name")
-//                    Log.d("TESTTEST1", "CONTACT_ID = $id")
-//                    Log.d("TESTTEST2", "CONTACT_PATH = ${getString(pathColumnIndex)}")
-//                    Log.d("TESTTEST3", "CONTACT_FAVORITE = ${getLong(isFavoriteColumnIndex)}")
-                }
-
-                // Close the cursor
-                close()
+                // Add the Plant object to the list
+                plantImageList.add(Plant(name = name, imagePath = contentUri, favorite = favorite, description = "", commonNames = listOf(), species = ""))
             }
 
-            // Emit the list of Plant objects
-            emit(plantImageList)
+            // Close the cursor
+            close()
         }
+
+        // Emit the list of Plant objects
+        _plantPicturesFlow.update { plantImageList }
     }
 }
 
