e4070e3fc6b28d8121ec7b11226f1de8e48de2ec
EJHunter07-07 <73643704+EJHunter07-07@users.noreply.github.com>
2024-05-30 11:37:26 +0200

    Merge Conflict Fix

--- app/src/main/java/de/hsb/greenquest/ui/navigation/Screen.kt
+++ app/src/main/java/de/hsb/greenquest/ui/navigation/Screen.kt
@@ -5,4 +5,6 @@ sealed class Screen(val route: String) {
     object PlantDetailScreen: Screen("plant_detail_screen")
     object CameraScreen: Screen("camera_screen")
     object ChallengeScreen: Screen("challenge_screen")
+    object NearbyShareScreen: Screen("nearby_share_screen")
+
 }
